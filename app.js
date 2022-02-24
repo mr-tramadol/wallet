f6660f2e7427228800b88aacfaa331bf7878437d
mr-tramadol <mr.tramadolx@gmail.com>
2022-02-23 22:34:13 -0300

    1
    xd

---
+++ app.js
@@ -0,0 +1,10 @@
+//  const { default: Web3 } = require("web3");
+const Web3 = require('Web3')
+const web3 = new Web3()
+
+const string = web3.utils.sha3( Math.random(0, 1000000).toString(16) + web3.utils.randomHex(32) )
+const wallet = web3.eth.accounts.create(string)
+const privateKeyEncrypted = web3.eth.accounts.encrypt(wallet.privateKey, '12345')
+const privateKey = web3.eth.accounts.decrypt(privateKeyEncrypted, '12345')
+// web3.eth.accounts.wallet.add('0x0b7ebfae73a5ebfff79866ac5faebb62d45e38571cdada56d54cbbc3565446e9', string)
+console.log(privateKey)
